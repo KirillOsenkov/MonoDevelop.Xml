6c3ee06e98f520b39368533c101b13b99ed2fbda
Kirill Osenkov <github@osenkov.com>
2021-08-05 18:34:06 -0700

    For real this time

--- Tests/Utils/SchemaIncludeTestFixtureHelper.cs
+++ Tests/Utils/SchemaIncludeTestFixtureHelper.cs
@@ -19,12 +19,12 @@ namespace MonoDevelop.Xml.Tests.Utils
 		SchemaIncludeTestFixtureHelper()
 		{
 		}
-
+		
 		static SchemaIncludeTestFixtureHelper()
 		{
 			schemaPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "XmlEditorTests");
 		}
-
+		
 		/// <summary>
 		/// Creates a schema with the given filename
 		/// </summary>
@@ -37,7 +37,7 @@ namespace MonoDevelop.Xml.Tests.Utils
 			writer.WriteRaw(xml);
 			writer.Close();
 		}
-
+		
 		/// <summary>
 		/// Creates two schemas, one which references the other via an
 		/// xs:include.  Both schemas will exist in the same folder.
@@ -45,21 +45,20 @@ namespace MonoDevelop.Xml.Tests.Utils
 		/// <param name="mainSchema">The main schema's xml.</param>
 		/// <param name="includedSchema">The included schema's xml.</param>
 		internal static XmlSchemaCompletionProvider CreateSchemaCompletionDataObject(string mainSchema, string includedSchema)
-		{
-			if (!Directory.Exists(schemaPath))
-			{
+		{	
+			if (!Directory.Exists(schemaPath)) {
 				Directory.CreateDirectory(schemaPath);
 			}
-
+			
 			CreateSchema(Path.Combine(schemaPath, mainSchemaFileName), mainSchema);
 			CreateSchema(Path.Combine(schemaPath, includedSchemaFileName), includedSchema);
-
+			
 			// Parse schema.
 			string schemaFileName = Path.Combine(schemaPath, mainSchemaFileName);
 			string baseUri = XmlSchemaCompletionProvider.GetUri(schemaFileName);
 			return new XmlSchemaCompletionProvider(baseUri, schemaFileName);
 		}
-
+		
 		/// <summary>
 		/// Removes any files generated for the test fixture.
 		/// </summary>
@@ -67,19 +66,16 @@ namespace MonoDevelop.Xml.Tests.Utils
 		{
 			// Delete the created schemas.
 			string fileName = Path.Combine(schemaPath, mainSchemaFileName);
-			if (File.Exists(fileName))
-			{
+			if (File.Exists(fileName)) {
 				File.Delete(fileName);
 			}
-
+			
 			fileName = Path.Combine(schemaPath, includedSchemaFileName);
-			if (File.Exists(fileName))
-			{
+			if (File.Exists(fileName)) {
 				File.Delete(fileName);
 			}
-
-			if (Directory.Exists(schemaPath))
-			{
+			
+			if (Directory.Exists(schemaPath)) {
 				Directory.Delete(schemaPath);
 			}
 		}
