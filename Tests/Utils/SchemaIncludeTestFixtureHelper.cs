43d1cb9f9a2e1e924def6925285e5be8b600cb6b
Kirill Osenkov <github@osenkov.com>
2021-08-05 18:33:38 -0700

    Undo changes to tests.

--- Tests/Utils/SchemaIncludeTestFixtureHelper.cs
+++ Tests/Utils/SchemaIncludeTestFixtureHelper.cs
@@ -19,12 +19,12 @@ namespace MonoDevelop.Xml.Tests.Utils
 		SchemaIncludeTestFixtureHelper()
 		{
 		}
-		
+
 		static SchemaIncludeTestFixtureHelper()
 		{
 			schemaPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "XmlEditorTests");
 		}
-		
+
 		/// <summary>
 		/// Creates a schema with the given filename
 		/// </summary>
@@ -44,19 +44,20 @@ namespace MonoDevelop.Xml.Tests.Utils
 		/// </summary>
 		/// <param name="mainSchema">The main schema's xml.</param>
 		/// <param name="includedSchema">The included schema's xml.</param>
-		internal static XmlSchemaCompletionProvider CreateSchemaCompletionDataObject (string mainSchema, string includedSchema)
+		internal static XmlSchemaCompletionProvider CreateSchemaCompletionDataObject(string mainSchema, string includedSchema)
 		{
-			if (!Directory.Exists (schemaPath)) {
-				Directory.CreateDirectory (schemaPath);
+			if (!Directory.Exists(schemaPath))
+			{
+				Directory.CreateDirectory(schemaPath);
 			}
 
-			CreateSchema (Path.Combine (schemaPath, mainSchemaFileName), mainSchema);
-			CreateSchema (Path.Combine (schemaPath, includedSchemaFileName), includedSchema);
+			CreateSchema(Path.Combine(schemaPath, mainSchemaFileName), mainSchema);
+			CreateSchema(Path.Combine(schemaPath, includedSchemaFileName), includedSchema);
 
 			// Parse schema.
-			string schemaFileName = Path.Combine (schemaPath, mainSchemaFileName);
-			string baseUri = XmlSchemaCompletionProvider.GetUri (schemaFileName);
-			return new XmlSchemaCompletionProvider (baseUri, schemaFileName);
+			string schemaFileName = Path.Combine(schemaPath, mainSchemaFileName);
+			string baseUri = XmlSchemaCompletionProvider.GetUri(schemaFileName);
+			return new XmlSchemaCompletionProvider(baseUri, schemaFileName);
 		}
 
 		/// <summary>
@@ -66,16 +67,19 @@ namespace MonoDevelop.Xml.Tests.Utils
 		{
 			// Delete the created schemas.
 			string fileName = Path.Combine(schemaPath, mainSchemaFileName);
-			if (File.Exists(fileName)) {
+			if (File.Exists(fileName))
+			{
 				File.Delete(fileName);
 			}
-			
+
 			fileName = Path.Combine(schemaPath, includedSchemaFileName);
-			if (File.Exists(fileName)) {
+			if (File.Exists(fileName))
+			{
 				File.Delete(fileName);
 			}
-			
-			if (Directory.Exists(schemaPath)) {
+
+			if (Directory.Exists(schemaPath))
+			{
 				Directory.Delete(schemaPath);
 			}
 		}
