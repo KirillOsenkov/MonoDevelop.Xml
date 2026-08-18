14535fd88c56a4045077f581708d727c92a74e2f
Kirill Osenkov <kirillo@microsoft.com>
2026-08-17 17:47:11 -0700

    Split tags for multiple carets

--- Editor/Commands/SplitTagsOnEnterCommandHandler.cs
+++ Editor/Commands/SplitTagsOnEnterCommandHandler.cs
@@ -2,6 +2,7 @@
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 using System;
+using System.Collections.Generic;
 using System.ComponentModel.Composition;
 
 using Microsoft.VisualStudio.Commanding;
@@ -62,46 +63,87 @@ namespace MonoDevelop.Xml.Editor.Commands
 
 		void ExecuteCommandInternal (ReturnKeyCommandArgs args, Action nextCommandHandler, CommandExecutionContext executionContext)
 		{
-			var caretPos = args.TextView.Caret.Position.BufferPosition;
-			int p = caretPos.Position;
-			var s = caretPos.Snapshot;
-
 			// we could be smarter about actually analyzing the XML and creating good undo transactions.
-			// right now it just looks for the caret being between "></" and moves the rest of the line
-			// onto an indented new line before letting the normal handler run
-			bool betweenTags = p > 0 && (p + 3) < s.Length && s[p - 1] == '>' && s[p] == '<' && s[p + 1] == '/';
-			if (!betweenTags) {
+			// right now it just looks for carets being between "></" and moves the rest of the line
+			// onto an indented new line before letting the normal handler run.
+			// every caret of a multi-selection is handled, then the carets are put back where they were
+			// so that the normal handler inserts its newline at each of them
+			var broker = args.TextView.GetMultiSelectionBroker ();
+			var selections = broker.AllSelections;
+			var s = args.SubjectBuffer.CurrentSnapshot;
+
+			var splitPositions = new List<int> ();
+			foreach (var selection in selections) {
+				int p = selection.InsertionPoint.Position.Position;
+				bool betweenTags = p > 0 && (p + 3) < s.Length && s[p - 1] == '>' && s[p] == '<' && s[p + 1] == '/';
+				if (betweenTags && !splitPositions.Contains (p)) {
+					splitPositions.Add (p);
+				}
+			}
+
+			if (splitPositions.Count == 0) {
 				nextCommandHandler ();
 				return;
 			}
 
+			splitPositions.Sort ();
+
+			// how much text was inserted at each split position (line break plus indent), for remapping the carets
+			var insertedLengths = new int[splitPositions.Count];
+
 			var undoManager = undoManagerProvider.GetTextBufferUndoManager (args.SubjectBuffer);
 			using (var transaction = undoManager.TextBufferUndoHistory.CreateTransaction ("Split Tags")) {
 				string lineBreakText = null;
-				var currentLine = s.GetLineFromPosition (p);
 				if (args.TextView.Options.GetReplicateNewLineCharacter ()) {
-					lineBreakText = currentLine.GetLineBreakText ();
+					lineBreakText = s.GetLineFromPosition (splitPositions[0]).GetLineBreakText ();
 				}
 				if (string.IsNullOrEmpty (lineBreakText)) {
 					lineBreakText = args.TextView.Options.GetNewLineCharacter ();
 				}
 
 				var edit = args.SubjectBuffer.CreateEdit ();
-				edit.Insert (p, lineBreakText);
+				foreach (int p in splitPositions) {
+					edit.Insert (p, lineBreakText);
+				}
 				s = edit.Apply ();
 
-				var nextLine = s.GetLineFromLineNumber (currentLine.LineNumber + 1);
-				var indent = smartIndentService.GetDesiredIndentation (args.TextView, nextLine);
-				if (indent != null) {
-					edit = args.SubjectBuffer.CreateEdit ();
-					edit.Insert (nextLine.Start.Position, GetIndentString (indent.Value, args.TextView.Options));
-					s = edit.Apply ();
+				// the new lines start after the line break; earlier splits shift the later ones
+				edit = args.SubjectBuffer.CreateEdit ();
+				int shift = 0;
+				for (int i = 0; i < splitPositions.Count; i++) {
+					insertedLengths[i] = lineBreakText.Length;
+					var nextLine = s.GetLineFromPosition (splitPositions[i] + shift + lineBreakText.Length);
+					var indent = smartIndentService.GetDesiredIndentation (args.TextView, nextLine);
+					if (indent != null) {
+						string indentString = GetIndentString (indent.Value, args.TextView.Options);
+						edit.Insert (nextLine.Start.Position, indentString);
+						insertedLengths[i] += indentString.Length;
+					}
+					shift += lineBreakText.Length;
 				}
+				s = edit.Apply ();
 				transaction.Complete ();
 			}
 
-			// move the caret back to the original location before letting the rest of the handlers run
-			args.TextView.Caret.MoveTo (new SnapshotPoint (s, p));
+			// move the carets back to their original locations before letting the rest of the handlers run
+			var newSelections = new List<Selection> (selections.Count);
+			int primaryIndex = 0;
+			for (int i = 0; i < selections.Count; i++) {
+				var selection = selections[i];
+				if (selection == broker.PrimarySelection) {
+					primaryIndex = i;
+				}
+
+				int p = selection.InsertionPoint.Position.Position;
+				int newPosition = p;
+				for (int j = 0; j < splitPositions.Count && splitPositions[j] < p; j++) {
+					newPosition += insertedLengths[j];
+				}
+
+				newSelections.Add (new Selection (new VirtualSnapshotPoint (new SnapshotPoint (s, newPosition))));
+			}
+
+			broker.SetSelectionRange (newSelections, newSelections[primaryIndex]);
 
 			nextCommandHandler ();
 		}
